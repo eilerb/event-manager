8eb944bf02ea6c242b63e6aa32cabfbd16f16109
eb <eiler.byberi@gmail.com>
2022-11-06 18:47:07 -0800

    Use the ERB library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -10,14 +11,11 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(", ")
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
@@ -26,6 +24,7 @@ end
 puts 'Event Manager Initialized!'
 
 template_letter = File.read('../form_letter.html')
+erb_template = ERB.new template_letter
 
 contents = CSV.open(
   '../event_attendees.csv', 
@@ -40,9 +39,7 @@ contents.each do |row|
 
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
-
+  form_letter = erb_template.result(binding)
   puts personal_letter
 end
 
