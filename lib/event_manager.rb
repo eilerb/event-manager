8d3a28a9cdc954640664e60c7b57618945e01289
eb <eiler.byberi@gmail.com>
2022-11-13 12:19:16 -0800

    Clean phone numbers

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,10 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(phone_number)
+  phone_number.to_s.rjust(10, '1')[0..9]
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -14,9 +18,9 @@ def legislators_by_zipcode(zip)
     civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-  rescue
+  rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
@@ -33,11 +37,11 @@ end
 
 puts 'Event Manager Initialized!'
 
-template_letter = File.read('../form_letter.html')
+template_letter = File.read('../form_letter.erb')
 erb_template = ERB.new template_letter
 
 contents = CSV.open(
-  '../event_attendees.csv', 
+  '../event_attendees.csv',
   headers: true,
   header_converters: :symbol
 )
@@ -48,11 +52,11 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
 
+  number = clean_phone_number(row[:homephone])
+
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id, form_letter)
 end
-
-
